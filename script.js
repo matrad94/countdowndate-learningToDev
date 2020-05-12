1f39b58dd8e03d3f0049997b9268f6e35a4454f4
Mateusz Radziwoński <matrad94@gmail.com>
2020-05-12 13:22:11 +0200

    added closing of each counters

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+});
+
 var t = 0;
 
 function dhms(ms){
@@ -11,26 +16,41 @@ function dhms(ms){
     return days+" dni, "+hours+" godzin, "+minutes+" minut, "+sec+" sekund";
 }
 
-function NewCountdown(date, tag, t){
-    this.id = t;
+function NewCountdown(date, tag, id){
+    this.id = id;
     this.date = date;
     this.tag = tag;
+    this.dif = date - Date.now();
+    let div = document.createElement("div");
+    div.setAttribute("class", "counter " + id + " card-panel hoverable blue lighten-5 center-align");
+    this.holder = document.getElementById("holder");
+    let h3 = document.createElement("h3");
+    let text = document.createElement("p");
+    let close = document.createElement("a");
+    close.setAttribute(`class`, "btn-floating btn-large waves-effect waves-light red");
+    close.innerHTML = `<i class="material-icons">close</i>`;
+    close.addEventListener('click', function(){div.parentNode.removeChild(div);});
+    this.div = div;
+    this.appendValues = function(){
+        text.innerHTML = "Pozostało: "+ dhms(this.dif);
+        h3.innerHTML = tag;
+    }
     this.addDiv = function(){
-        var a = document.createElement("div");
-        a.setAttribute("class", "counter " + t + " card-panel hoverable blue lighten-5 center-align");
-        var b = document.getElementById("holder");
-        a.innerHTML = " ";
-        b.appendChild(a);
-        return a;
+        div.appendChild(h3);
+        div.appendChild(text);
+        div.appendChild(close);
+        holder.appendChild(div);
+        return div;
     }
     this.contdown = function() {
-        var a = this.addDiv();
+        var div = this.addDiv();
+        this.appendValues();
         var date = this.date;
         setInterval(function () {
-            dif = date - Date.now();
-            a.innerHTML = "<h3>" + tag + "</h3><p>Pozostało: " + dhms(dif) + "</p>";
+            this.dif = date - Date.now();
+            text.innerHTML = "Pozostało: "+ dhms(this.dif);
             if (dif < 0) {
-                a.innerHTML = "<h2>KONIEC!</h2>";
+                div.innerHTML = "<h2>KONIEC!</h2>";
                 return;
             }
         }, 1000);
